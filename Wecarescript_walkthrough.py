9a6ac1acac3c643971cd097ce53f333ce1517770
vishnuwe45 <87526447+vishnuwe45@users.noreply.github.com>
2022-04-07 10:28:22 +0530

    Update Wecarescript_walkthrough.py

--- Wecarescript_walkthrough.py
+++ Wecarescript_walkthrough.py
@@ -244,7 +244,7 @@ def run_zap_active_scan():
 def export_zap_report_of_scan():
     try:
         zap_handler.zap_export_report("CTF_walkthrough_zap_scan_result.xml","xml","CTF_walkthrough_zap_scan_result.xml","Vishnuwe45")
-        zap_handler.zap_export_report("CTF_walkthrough_zap_scan_result.json","json","CTF_walkthrough_zap_scan_result.html","Vishnuwe45")
+        zap_handler.zap_export_report("CTF_walkthrough_zap_scan_result.json","json","CTF_walkthrough_zap_scan_result.json","Vishnuwe45")
     except Exception as e:
         print(e)
 
