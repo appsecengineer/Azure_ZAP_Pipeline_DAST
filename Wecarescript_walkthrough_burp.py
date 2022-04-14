80c301d08cfe12fa6e7097b6baecf658792dddce
vishnuwe45 <87526447+vishnuwe45@users.noreply.github.com>
2022-04-14 21:16:37 +0530

    Update Wecarescript_walkthrough_burp.py

--- Wecarescript_walkthrough_burp.py
+++ Wecarescript_walkthrough_burp.py
@@ -13,6 +13,7 @@ def run_burp_in_headless_mode():
         user_config = "user_options.json"
         project_config = "project.json"
         burp_handler.start_headless_burpsuite(path,user_config=user_config,project_config=project_config)
+        #burp_handler.start_headless_burpsuite(path,user_config=user_config)
         sleep(60)
     except Exception as e:
         print(e)
